0bdcfbf1edf90326d69ccdc3c53a0fc9309d2604
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2021-09-03 14:16:43 +0300

    chall8.py

---
+++ chall8.py
@@ -0,0 +1,10 @@
+# In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+# Function Description
+
+# Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+# aVeryBigSum has the following parameter(s):
+
+def aVeryBigSum(ar):
+   return sum(ar)
