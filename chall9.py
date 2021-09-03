2cda9fe0e0a3a8f20a7a71ccd8b41dbbcf8f1b0e
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2021-09-03 20:46:07 +0300

    adding chall9

--- chall9.py
+++ chall9.py
@@ -6,20 +6,10 @@
 # 4 5 6
 # 9 8 9  
 
-def diagonalDifference(arr, n):
-    d1 = 0
-    d2 = 0
-    for i in range (0, n):
-        for  j in range (0, n):
-            if i == j:
-                d1 += arr[i,j]
-            if i == n - j - 1:
-                d2 += arr[i,j]    
-    return abs(d1 - d2);
-
-n = 3
-arr = [[11, 2, 4],
-       [4 , 5, 6],
-       [10, 8, -12]]
-            
-print(diagonalDifference(arr,n))            
+def diagonalDifference(arr):
+    my_sum1 = 0
+    my_sum2 = 0
+    for i in range(len(arr)):
+        my_sum1 += arr[i][i]
+        my_sum2 += arr[i][len(arr)-i-1]
+    return abs(my_sum1-my_sum2)
