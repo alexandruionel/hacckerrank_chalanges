3498fc705225120d3cbd7a2acef83c2452604e6a
alexandruionel <57631428+alexandruionel@users.noreply.github.com>
2021-09-03 11:56:17 +0300

    update chall 6

---
+++ chall6.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = int(input())
+b = float(input())
+c = str(input())
+# Read and save an integer, double, and String to your variables.
+print(a, b, c, sep ="\n")
+
+# Print the sum of both integer variables on a new line.
+print(i + a)
+# Print the sum of the double variables on a new line.
+print(d + b)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + c)
